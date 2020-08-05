dc2796e1dca6402d6c257da198302d88ba5a5244
s1270126-Yamamoto <s1270126@u-aizu.ac.jp>
2020-08-06 03:48:20 +0900

    Victory message added

--- ex4.c
+++ ex4.c
@@ -6,7 +6,15 @@ int main(){
 	int i;
 	int heads = 0;
 	int tails = 0;
-
+	char name[20];
+	
+	//Asking name and greeting
+	printf("Who are you?\n> ");
+	fflush(stdout);
+	scanf("%s", name);
+	printf("Hello, %s!\n", name);
+	
+	//Heads or Tails
 	srand((unsigned int)time(NULL));
 
 	printf("Tossing a coin...\n");
@@ -22,6 +30,10 @@ int main(){
 		}
 	}
 	printf("Heads: %d, Tails: %d\n", heads, tails);
-
+	
+	//Judgment
+	if(heads > tails) printf("%s won!\n", name);
+	else printf("%s lost!\n", name);
+	
 	return 0;
 }
